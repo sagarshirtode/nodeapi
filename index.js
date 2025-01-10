b5e11a3f70858e45e737345d9497921f92791322
sagarshirtode112 <sagarshirtode112@gmail.com>
2025-01-10 10:41:11 +0530

    add sample user data for defalut api

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const data = require("./data.json");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, resp) => {
-  resp.json({ title: "NodeAPI", message: "Success", status: 200, data: [] });
+  resp.json({ title: "NodeAPI", message: "Success", status: 200, data: data });
 });
 
 app.listen(port, () => {
